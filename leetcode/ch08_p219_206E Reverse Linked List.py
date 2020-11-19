51982ce892fb7f409dcc0d8cdc10144aa29505a8
kio <yko2222@gmail.com>
2020-11-19 14:02:31 +0900

    ch08

--- leetcode/ch08_p219_206E Reverse Linked List.py
+++ leetcode/ch08_p219_206E Reverse Linked List.py
@@ -1,4 +1,4 @@
-'''
+  '''
 Reverse a singly linked list.
 
 Example:
@@ -11,6 +11,8 @@ A linked list can be reversed either iteratively or recursively. Could you imple
 
 '''
 from typing import *
+from copy import copy
+from collections import deque
 
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -18,4 +20,28 @@ class ListNode:
         self.next = next
 
 class Solution:
+    # 내 풀이 (막장) - 40ms, 26.21%
     def reverseList(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+        mylist = deque()
+        result = None
+        while head:
+            mylist.append(head.val)
+            head = head.next
+        while mylist:
+            if result is None:
+                result = ListNode(val=mylist.popleft())
+                continue
+            result.val, result.next = mylist.popleft(), copy(result)
+        return result
+
+    def reverseList2(self, head: ListNode) -> ListNode:
+    # 책 풀이 (1) 재귀 - 40ms, 26.21%
+        def reverse(head: ListNode, result: ListNode = None) -> ListNode:
+            if not head:
+                return result
+            new, head.next = head.next, result
+            return reverse(new, head)
+
+        return reverse(head)
